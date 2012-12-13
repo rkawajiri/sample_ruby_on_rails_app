cf8d4a33eeb2cb78f1b67bfd05f56aa386e3722f
Ryoma Kawajiri <ryoma.edison@gmail.com>
2012-12-13 15:06:16 +0900

    refactored to destroy action to prevent admin users from destroyingthemselves

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user, :only => :destroy
+  before_filter :not_oneself, :only => :destroy
 
   def index
     @title = "All users"
@@ -51,15 +52,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:id])
-
-    if current_user == user
-      redirect_to(root_path)
-    else 
-      user.destroy
-      flash[:success] = "User destroyed."
-      redirect_to users_path
-    end
+    @user.destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
   end
 
   private
@@ -75,5 +70,10 @@ class UsersController < ApplicationController
 
   def admin_user
     redirect_to(root_path) unless current_user.admin?
+  end
+
+  def not_oneself
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user != @user
   end
 end
